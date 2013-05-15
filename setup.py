0e65bbafb8ffa623d8b99294025b33489b9fcad8
Jonathan Reed <jdreed@mit.edu>
2013-05-15 16:10:31 -0400

    Add setup.py for omega module

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="PyOmega",
+    version="1.0",
+    description="PyOmega - Python module to emulate Pharos Omega",
+    author="Jonathan Reed",
+    author_email="jdreed@mit.edu",
+    license="GPL2",
+    py_modules=['omega'],
+)
