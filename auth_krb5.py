205c677d9ed140b6702fe292079038c5a97b41cd
Jonathan Reed <jdreed@mit.edu>
2013-05-16 10:53:42 -0400

    Add new auth_krb5.py module Depend on new python-omega package Depend on python-pam for auth_krb5 Add pam.d entry for auth_krb5 Update paths in .install file

---
+++ auth_krb5.py
@@ -0,0 +1,49 @@
+import PAM
+import sys
+import logging
+
+# This module uses logging.  To enable it, do something like this in the 
+# caller:
+# import logging
+# logging.basicConfig()
+# auth_krb5.logger.setLevel(logging.DEBUG)
+
+logger = logging.getLogger('auth_krb5')
+logger.addHandler(logging.NullHandler())
+
+class Authenticator():
+
+    def __init__(self, pam_service='python-auth_krb5'):
+        self.pam = PAM.pam()
+        self.pam_service = pam_service
+        self.debugPrintPassword = False
+        self.error = ''
+
+    def _pam_conversation(self, pam, msgs, response):
+        # response unused in this implementation
+        # Must return an array of tuples
+        logger.debug("PAM Message: %s", msgs)
+        if len(msgs) == 1 and msgs[0][1] == PAM.PAM_PROMPT_ECHO_OFF:
+            return [(self.password,0)]
+        else:
+            logger.warn("Unexpected PAM response: ", msgs)
+            self.error = msgs[0][0]
+            return None
+        
+
+    def authenticate(self, user, password):
+        logger.debug("authenticate(), user=%s, password=%s", 
+                     user, 
+                     password if self.debugPrintPassword else "[redacted]")
+        self.password = password
+        self.pam.start(self.pam_service, user, self._pam_conversation)
+        try:
+            self.pam.authenticate()
+            logger.debug("Authentication successful")
+        except PAM.error as error:
+            logger.debug("Authentication failed: %s", error)
+            self.error = error[0]
+            return False
+        else:
+            return True
+        
